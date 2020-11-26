e1d639a09af135945d24d688a5477efc010d9507
ChayanRin <k852012@naver.com>
2020-11-26 20:00:17 +0900

    feat: firebase 기본 설정 작성

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import Header from "components/Header";
 import Contents from "components/Contents";
 
+import { authService } from "./blogFirebase";
+
 function App() {
   const [loggedIn, setLoggedIn] = useState(true);
 
---
+++ src/blogFirebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
+
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DBURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MSID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
+export const storageService = firebase.storage();
