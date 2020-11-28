a8149396553e4da0f7c0248f8acc4f5e1d2bcdd3
ChayanRin <k852012@naver.com>
2020-11-28 15:47:59 +0900

    feat: nav css 작업 및 nav 좌측 정렬

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,15 @@ import { Link } from "react-router-dom";
 const Header = ({ loggedIn }) => {
   return (
     <div className="header">
-      <div className="header__column header-logo">
-        <Link to="/">
-          <img src="https://firebasestorage.googleapis.com/v0/b/chayanblog.appspot.com/o/static%2Flogo.svg?alt=media&token=1506f45c-8be3-40b1-8bb1-2c55f750109e" />
-        </Link>
-      </div>
       <div className="header__column">
+        <div className=" header-logo">
+          <Link to="/">
+            <img src="https://firebasestorage.googleapis.com/v0/b/chayanblog.appspot.com/o/static%2Flogo.svg?alt=media&token=1506f45c-8be3-40b1-8bb1-2c55f750109e" />
+          </Link>
+        </div>
         <Nav />
       </div>
+
       <div className="header__column">
         {loggedIn ? <div className="write-post">새 글 작성</div> : null}
 
