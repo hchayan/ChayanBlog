adf7fe1ab690b96ba25c55526fcb8e4f0de8aed9
ChayanRin <k852012@naver.com>
2020-11-29 19:05:09 +0900

    fix: 라우터 개선

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Header from "components/Header";
 import Contents from "components/Contents";
 
-import Login from "components/page/Auth";
+import Auth from "components/page/Auth";
 
 import { authService } from "./blogFirebase";
 
@@ -26,19 +31,23 @@ function App() {
     <div className="App">
       {init ? (
         <Router>
-          <Route exact path="/">
-            <Header loggedIn={loggedIn} />
-            <Contents />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/register">
-            register
-          </Route>
-          <Route exact path="/404">
-            404
-          </Route>
+          <Switch>
+            <Route exact path="/">
+              <Header loggedIn={loggedIn} />
+              <Contents />
+            </Route>
+
+            <Route exact path="/404">
+              404
+            </Route>
+
+            {loggedIn ? (
+              <Route exact path="/register">
+                register
+              </Route>
+            ) : null}
+            <Redirect from="*" to="/" />
+          </Switch>
         </Router>
       ) : (
         <div className="Loading">페이지 로딩중...</div>
