249d7df3037b39d489509f5d53bcb8089aae3e32
ChayanRin <k852012@naver.com>
2020-12-01 03:57:14 +0900

    feat: 기본 thubnail 업로드 html, css 작성

--- src/components/page/Write.js
+++ src/components/page/Write.js
@@ -8,10 +8,48 @@ const Write = () => {
   ### test
   `);
 
+  const [tags, setTags] = useState([]);
+
+  const [categories, setCategories] = useState(["1", "2", "3"]);
+
+  const onSubmit = () => {};
+
   return (
     <div className="write">
       <div className="write__column">
-        <MDEditor value={markdownContent} onChange={setMarkdownContent} />
+        <div className="write-form">
+          <form onSubmit={onSubmit}>
+            <div className="write-form__column">
+              <div className="write-thumbnail">
+                <div className="thumbnail-preview">
+                  <label htmlFor="write-thumbnail">썸네일 업로드</label>
+                  <input type="file" id="write-thumbnail" accept="image/*" />
+                </div>
+              </div>
+            </div>
+            <div className="write-form__column">
+              <div className="form-title">
+                <input type="text" placeholder="제목을 입력하세요" />
+              </div>
+
+              <div className="write-tags">
+                <div className="tags-lists">{tags}</div>
+                <div className="write-add-tags">태그 추가</div>
+              </div>
+              <div className="write-category">
+                <div className="cate-lists">{categories}</div>
+                <div className="write-cate-tags">카테고리 추가</div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="write__column">
+        <MDEditor
+          value={markdownContent}
+          onChange={setMarkdownContent}
+          height={810}
+        />
       </div>
     </div>
   );
