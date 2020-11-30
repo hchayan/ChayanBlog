041d120edbadd4f31aee80bc05ec558489038858
ChayanRin <k852012@naver.com>
2020-11-30 17:40:16 +0900

    feat: 닉네임 변경 구현

--- src/components/popup/ProfilePopup.js
+++ src/components/popup/ProfilePopup.js
@@ -1,9 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { authService } from "blogFirebase";
 
 const ProfilePopup = ({ userObj }) => {
   const postCount = 0;
   const commentCount = 0;
+
+  // 프로필 닉네임 수정
+  const [nameChangeState, setNameChangeState] = useState(false);
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewDisplayName(value);
+  };
+
+  const onClickNameChange = () => {
+    setNameChangeState(!nameChangeState);
+  };
+
+  const onSubmitName = async (event) => {
+    event.stopPropagation();
+
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+
+    setNameChangeState(!nameChangeState);
+  };
 
   // 로그아웃
   const onLogOutClick = () => authService.signOut();
@@ -25,8 +52,22 @@ const ProfilePopup = ({ userObj }) => {
       <div className="profile-popup__column">
         <div className="profile-info">
           <div className="profile-name">
-            <p>{userObj ? userObj.displayName : "익명"}</p>
-            <button className="profile-name--edit">수정</button>
+            {nameChangeState ? (
+              <form onSubmit={onSubmitName}>
+                <input
+                  onChange={onChange}
+                  type="text"
+                  placeholder="닉네임"
+                  value={newDisplayName}
+                />
+                <button className="profile-name--edit">변경</button>
+              </form>
+            ) : (
+              <form onSubmit={onClickNameChange}>
+                <p>{userObj ? userObj.displayName : "익명"}</p>
+                <button className="profile-name--edit">수정</button>
+              </form>
+            )}
           </div>
           <div className="profile-email">{userObj ? userObj.email : null}</div>
           <div className="profile-postInfo">
