d22e30389b1a2aebe5cb3f1630367bbedb74c209
ChayanRin <k852012@naver.com>
2020-12-20 13:58:17 +0900

    feat: previewMenu 기본 구성 구현

--- src/components/preview/PreviewMenu.js
+++ src/components/preview/PreviewMenu.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { HashLink as Link } from "react-router-hash-link";
 
 const PreviewMenu = () => {
-  return <div className="preview__menu">preview__menu</div>;
+  const [menuNav, setMenuNav] = useState([
+    { name: "전체", tag: "all" },
+    { name: "포스트", tag: "post" },
+    { name: "기본개념", tag: "basic" },
+    { name: "클론코딩", tag: "clone" },
+    { name: "태그", tag: "tag" },
+  ]);
+
+  return (
+    <div className="preview-menu">
+      <div className="preview-menu__column">
+        <div className="preview-menu-nav">
+          <ul>
+            {menuNav.map(menu => {
+              return (
+                <li>
+                  <Link to={`/category/${menu.tag}`}>{menu.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="preview-menu__column">
+        <div className="search-bar">
+          <form>
+            <input tpye="text" placeholder="원하는 내용을 검색하세요" />
+            <input type="submit" value="검색" />
+          </form>
+        </div>
+        <div className="menu-more"></div>
+      </div>
+    </div>
+  );
 };
 
 export default PreviewMenu;
