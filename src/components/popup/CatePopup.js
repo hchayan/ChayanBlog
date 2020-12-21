69c0b554aaf7b4da3c919bf5193799badb44ca7b
ChayanRin <k852012@naver.com>
2020-12-21 18:22:20 +0900

    feat: 카테고리 목록 불러오기, 추가 기능 구현

--- src/components/popup/CatePopup.js
+++ src/components/popup/CatePopup.js
@@ -1,35 +1,64 @@
 import { dbService } from "blogFirebase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const CatePopup = ({ cates, setCates }) => {
-  const [dbCates, setdbCates] = useState([
-    "일상",
-    "외부포스트",
-    "HTML 기본개념",
-    "CSS 기본개념",
-    "JS 기본개념",
-    "React 기본개념",
-    "유투브 클론코딩",
-    "블로그 구축 토이 프로젝트",
-  ]);
+  const [inputCategory, setInputCategory] = useState("");
+  const [dbCates, setdbCates] = useState([]);
 
-  const addCate = async e => {
+  const onChangeAddCategory = e => {
+    setInputCategory(e.target.value);
+  };
+
+  const addCategoryOnPost = async e => {
     e.preventDefault();
-
     if (!cates.includes(e.target.value)) {
       setCates([...cates, e.target.value]);
     }
   };
 
+  const addDBCategory = async e => {
+    e.preventDefault();
+    await dbService
+      .collection("statics")
+      .doc("categories")
+      .set({
+        name: [...dbCates, inputCategory],
+      });
+
+    setdbCates([...dbCates, inputCategory]);
+    setInputCategory("");
+  };
+
+  const loadDBCategory = async () => {
+    const dbCategories = await dbService
+      .collection("statics")
+      .doc("categories")
+      .get();
+
+    setdbCates(dbCategories.data().name);
+    console.log(dbCates);
+  };
+
+  useEffect(() => {
+    loadDBCategory();
+  }, []);
+
   return (
     <div className="cates-popup">
       <div className="cates-popup__column">
-        <input type="text" placeholder="추가할 카테고리를 적어주세요" />
-        <input type="button" value="추가" />
+        <input
+          type="text"
+          value={inputCategory}
+          onChange={onChangeAddCategory}
+          placeholder="추가할 카테고리를 적어주세요"
+        />
+        <input type="button" value="추가" onClick={addDBCategory} />
       </div>
       <div className="cates-popup__column cates-list--btn">
         {dbCates.map(cate => {
-          return <input type="button" value={cate} onClick={addCate} />;
+          return (
+            <input type="button" value={cate} onClick={addCategoryOnPost} />
+          );
         })}
       </div>
     </div>
