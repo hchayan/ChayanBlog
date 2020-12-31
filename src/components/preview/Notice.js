a61d92f2e8969adeffc4ed4efa47d3624c14b38e
ChayanRin <k852012@naver.com>
2020-12-31 09:29:35 +0900

    feat: notice 컨텐츠 구현

--- src/components/preview/Notice.js
+++ src/components/preview/Notice.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Notice = () => {
-  return <div className="notice"></div>;
+  return (
+    <div
+      className="notice"
+      style={{
+        backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/chayanblog.appspot.com/o/static%2Fbanner.jpg?alt=media&token=b11b37c2-5652-4787-83a6-fec0c17d2cc1")`,
+      }}
+    ></div>
+  );
 };
 
 export default Notice;
