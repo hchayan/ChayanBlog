57b49f519f59a7d34201b3194f522fe886fd38e1
ChayanRin <k852012@naver.com>
2021-01-01 21:13:14 +0900

    feat: 주소 hashrouter 방식 교체

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
