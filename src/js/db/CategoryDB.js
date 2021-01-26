ccce4c6388d1b1d4e0555a63a5f5875bb144e832
ChayanRin <k852012@naver.com>
2021-01-27 03:19:28 +0900

    chore: reset project folder

---
+++ src/js/db/CategoryDB.js
@@ -0,0 +1,31 @@
+import { dbService } from "../../blogFirebase";
+
+const saveDBCategory = async categories => {
+  await dbService.collection("statics").doc("categories").set({
+    name: categories,
+  });
+};
+
+const addDBCategory = async (categories, inputCategory) => {
+  await dbService
+    .collection("statics")
+    .doc("categories")
+    .set({
+      name: [...categories, inputCategory],
+    });
+};
+
+const loadDBCategory = async () => {
+  try {
+    const dbCategories = await dbService
+      .collection("statics")
+      .doc("categories")
+      .get();
+
+    return dbCategories.data().name;
+  } catch (error) {
+    return [];
+  }
+};
+
+export { saveDBCategory, addDBCategory, loadDBCategory };
